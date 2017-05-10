d6c50ea4ad69294f0f68bc872596c7bbabcb1d58
root <tarasov.p.b@gmail.com>
2017-05-10 18:32:45 +0000

    final

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -2,27 +2,27 @@ import tensorflow as tf
 tf.python.control_flow_ops = tf
 
 import os
-import pandas as pd
+
+os.environ['CUDA_VISIBLE_DEVICES']=str(0)
+
+import matplotlib.image as mpimg
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 import numpy as np
-import cv2
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Dropout, \
-MaxPooling2D, Conv2D, Lambda, Cropping2D, Convolution2D,\
-AveragePooling2D
-from keras.callbacks import History,TensorBoard, EarlyStopping, ModelCheckpoint
-from sklearn.cross_validation import train_test_split
-
-from copy import deepcopy
-from sklearn.utils import shuffle
-
-
-from generator import generator
-from get_images import get_images
-
+import csv
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, ConvLSTM2D, Dense, MaxPooling2D, Dropout, Flatten, Reshape, merge, Input
 from keras.optimizers import Adam
+from keras_tqdm import TQDMNotebookCallback
+from generator import generator,generator2
 from car_models import *
+from sklearn.utils import shuffle
+from tqdm import tqdm_notebook
 
 
+
+
+from get_images import get_images
 
 datadirs=['/notebooks/udacity/new_training/map1_backward/',
                  '/notebooks/udacity/new_training/map1_forward/',
@@ -36,48 +36,51 @@ datadirs=['/notebooks/udacity/new_training/map1_backward/',
                    '/notebooks/udacity/new_training/map2_error_correction/'
          ]
 
-images=get_images(datadirs,0.1)
-
-#different preprocessing methods
-size=(40,80)
+images=get_images(datadirs,0.08)
+image_names_full, y_data_full = images.img.values, images.real.values
 
 
-names, steering, inverse = images.img.values, images.real.values, images.inverse.values
+def proc_img(img): # input is 160x320x3
+    img = img[59:138:2, 0:-1:2, :] # select vertical region and take each second pixel to reduce image dimensions
+    img = (img / 127.5) - 1.0 # normalize colors from 0-255 to -1.0 to 1.0
+    return img # return 40x160x3 image
+
+names_train, names_valid, y_train, y_valid = train_test_split(image_names_full, y_data_full, test_size=0.02,\
+                            random_state=0)
+
+inverse_train=[0 for i in y_train]+[1 for i in y_train]
 
 
-names_train, names_valid, steering_train, steering_valid, inverse_train, inverse_valid = \
-    train_test_split(names, steering, inverse, test_size=0.2)
+names_train=  np.concatenate([names_train,names_train])
+y_train    =   np.concatenate([y_train,y_train])
+
+names_train,y_train,inverse_train=shuffle(names_train,y_train,inverse_train) 
 
 
-model=modified_vgg()
 
-checkpoint = ModelCheckpoint('model.h5', monitor='val_mean_squared_error', verbose=1,
-                              save_best_only=True, mode='min')
-early_stop = EarlyStopping(monitor='val_mean_squared_error',\
-                               min_delta=0.001, patience=3,
-                                verbose=1, mode='min')
 
-#for validation - only first lap
-lap1=[i for i in range(len(names_valid)) if 'map1' in names_valid[i]]
-valid_gen_lap1=generator (names_valid[lap1], steering_valid[lap1], batch_size=128)
+train_gen=generator2(names_train, y_train, batch_size=64, preprocessing=proc_img, inverse=inverse_train)
+valid_gen=generator(names_valid, y_valid, batch_size=64, preprocessing=proc_img)
 
-train_gen=generator(names_train, steering_train, batch_size=128)
 
 tb=TensorBoard(log_dir='logs', histogram_freq=1, write_graph=True, write_images=True)
 
 history = History()
 
-model.compile(loss='mse', optimizer='adam', metrics=['mean_squared_error'])
+model=modified_vgg()
+model.compile(loss='mse', optimizer=Adam(lr=0.0001), metrics=['mean_squared_error'])
  
 
-q=model.fit_generator(train_gen, \
+hidtory=model.fit_generator(train_gen, \
                     samples_per_epoch=len(names_train),\
-                    nb_epoch=100,\
-                    verbose=1,\
-                    validation_data=valid_gen_lap1, \
-                    nb_val_samples=len(lap1),\
-                    callbacks=[checkpoint, early_stop,history,tb]\
+                    nb_epoch=10,\
+                    verbose=True,\
+                    validation_data=valid_gen, \
+                    nb_val_samples=len(names_valid),\
+                    callbacks=[tb]\
                    )
+
+
     
 f=open('history.pk1','wb')
 pickle.dump(history.history,f,-1)
