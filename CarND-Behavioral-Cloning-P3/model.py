2b3ef3352a5b0d2d1ea79e62d0f6997a3b926b0b
Тарасов Павел <tarasov.p.b@gmail.com>
2017-05-12 17:35:27 +0300

    clean up

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -39,15 +39,17 @@ datadirs=['/notebooks/udacity/new_training/map1_backward/',
 images=get_images(datadirs,0.08)
 image_names_full, y_data_full = images.img.values, images.real.values
 
-
+#preprocessing function
 def proc_img(img): # input is 160x320x3
     img = img[59:138:2, 0:-1:2, :] # select vertical region and take each second pixel to reduce image dimensions
     img = (img / 127.5) - 1.0 # normalize colors from 0-255 to -1.0 to 1.0
     return img # return 40x160x3 image
 
+#generating train/valid sets
 names_train, names_valid, y_train, y_valid = train_test_split(image_names_full, y_data_full, test_size=0.02,\
                             random_state=0)
 
+#for each image adding inverse image in train set
 inverse_train=[0 for i in y_train]+[1 for i in y_train]
 
 
@@ -58,7 +60,7 @@ names_train,y_train,inverse_train=shuffle(names_train,y_train,inverse_train)
 
 
 
-
+#generators
 train_gen=generator2(names_train, y_train, batch_size=64, preprocessing=proc_img, inverse=inverse_train)
 valid_gen=generator(names_valid, y_valid, batch_size=64, preprocessing=proc_img)
 
@@ -70,7 +72,7 @@ history = History()
 model=modified_vgg()
 model.compile(loss='mse', optimizer=Adam(lr=0.0001), metrics=['mean_squared_error'])
  
-
+#fitting model
 hidtory=model.fit_generator(train_gen, \
                     samples_per_epoch=len(names_train),\
                     nb_epoch=10,\
